9d9a23c3db425323142f7014232b7ede251adb2a
Lemincr <Delsinne9@gmail.com>
2026-01-03 00:14:33 +0100

    Create analyse.h

---
+++ analyse.h
@@ -0,0 +1,9 @@
+#ifndef ANALYSE_H
+#define ANALYSE_H
+#include "grille.h"
+
+int count_alive(Grid *g); [cite: 67]
+void show_stats(Grid *g); [cite: 69]
+int check_stable(Grid *g1, Grid *g2); [cite: 70]
+
+#endif
