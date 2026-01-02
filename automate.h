22d7b9718b7cffbdb48c67edc9eeb8db7e070b9b
Justeunbrider83 <dorianrobert83590@gmail.com>
2026-01-03 00:17:43 +0100

    create automate.h

---
+++ automate.h
@@ -0,0 +1,8 @@
+#ifndef AUTOMATE_H
+#define AUTOMATE_H
+#include "grille.h"
+
+int count_neighbors(Grid *g, int l, int c); [cite: 63]
+void apply_rules(Grid *current, Grid *next); [cite: 64, 65]
+
+#endif
